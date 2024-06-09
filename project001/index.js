3e2fb2c7b107ad25a333b0f4eb3fd1f67f2bfb9a
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-10 00:20:34 +0300

    commit

--- project001/index.js
+++ project001/index.js
@@ -1,1 +1,11 @@
-document.firstElementChild.firstElementChild;
+let h1 = document.firstElementChild.lastElementChild;
+
+h1.textContent = "Goodbye World!";
+h1.style.color = "green";
+h1.style.fontSize = "50px";
+h1.style.textAlign = "center";
+h1.style.fontFamily = "sans-serif";
+h1.style.fontWeight = "bold";
+h1.style.textTransform = "uppercase";
+
+console.log(h1);
