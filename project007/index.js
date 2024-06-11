f55ab8f3a5aa4d98a09b89282d1218bf4de30f83
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 08:42:05 +0300

    Commit

--- project007/index.js
+++ project007/index.js
@@ -7,3 +7,18 @@ console.log(b.length);
 
 let c = a.getAttribute("href");
 console.log(c);
+
+a.setAttribute("href", "https://www.clarusway.com");
+console.log(a.getAttribute("href"));
+
+a.setAttribute("target", "_blank");
+console.log(a.getAttribute("target"));
+
+a.removeAttribute("target");
+console.log(a.getAttribute("target"));
+
+a.setAttribute("target", "_blank");
+console.log(a.getAttribute("target"));
+
+a.setAttribute("href", "https://www.clarusway.com");
+console.log(a.getAttribute("href"));
