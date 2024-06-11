0ea1725f90bba29d8990227750154fa8308156b1
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 08:35:07 +0300

    Commit

---
+++ project007/index.js
@@ -0,0 +1,9 @@
+let a = document.querySelector("a");
+console.log(a);
+console.log(a.textContent);
+let b = a.attributes;
+console.log(b);
+console.log(b.length);
+
+let c = a.getAttribute("href");
+console.log(c);
