9ad0a363248dbb8707cdc7eecc4666ddf20c73a4
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 10:39:01 +0300

    Commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,57 +1,81 @@
-const drumButtons = document.querySelectorAll(".drum");
-const numberOfDrumButtons = drumButtons.length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-// Add click event listeners to each drum button
-for (let i = 0; i < numberOfDrumButtons; i++) {
-  drumButtons[i].addEventListener("click", function () {
-    const buttonInnerHTML = this.innerHTML;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
     makeSound(buttonInnerHTML);
+
     buttonAnimation(buttonInnerHTML);
+
   });
+
 }
 
-// Add keydown event listener to the document
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keypress", function(event) {
+
   makeSound(event.key);
+
   buttonAnimation(event.key);
+
 });
 
+
 function makeSound(key) {
+
   switch (key) {
     case "w":
-      new Audio("sounds/tom-1.mp3").play();
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
+
     case "a":
-      new Audio("sounds/tom-2.mp3").play();
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
       break;
+
     case "s":
-      new Audio("sounds/tom-3.mp3").play();
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
       break;
+
     case "d":
-      new Audio("sounds/tom-4.mp3").play();
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
       break;
+
     case "j":
-      new Audio("sounds/snare.mp3").play();
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
       break;
+
     case "k":
-      new Audio("sounds/crash.mp3").play();
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
       break;
+
     case "l":
-      new Audio("sounds/kick-bass.mp3").play();
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
       break;
-    default:
-      console.log(key); // Log key if it's not a valid drum key
-      return; // Exit function if no valid key
+
+
+    default: console.log(key);
+
   }
 }
 
+
 function buttonAnimation(currentKey) {
-  const activeButton = document.querySelector("." + currentKey);
-  if (activeButton) {
-    // Check if activeButton exists
-    activeButton.classList.add("pressed");
-    setTimeout(function () {
-      activeButton.classList.remove("pressed");
-    }, 100);
-  }
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
