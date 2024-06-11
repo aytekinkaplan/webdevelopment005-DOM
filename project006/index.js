1f016390bef68b4b162380c973736013aa24fd6a
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 08:35:26 +0300

    Commit

--- project006/index.js
+++ project006/index.js
@@ -1,5 +1,12 @@
-document.querySelector("button").addEventListener("click", function () {
-  document.querySelector("h1").textContent = "Goodbye!";
-  document.querySelector("h1").style.color = "blue";
-  document.querySelector("h1").style.fontSize = "60px";
-});
+let a = document.querySelector("a");
+console.log(a);
+console.log(a.textContent);
+
+let b = (a.textContent = "Hello World");
+console.log(b);
+
+let c = document.querySelector("h1");
+console.log(c);
+console.log(c.textContent);
+c.textContent = "Hello World";
+console.log(c.textContent);
