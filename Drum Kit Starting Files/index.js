70fbdc8c58701a702404bfa0a5fdd05eeb1f67d3
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 10:07:49 +0300

    Commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,51 +1,52 @@
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const drumButtons = document.querySelectorAll(".drum");
+const numberOfDrumButtons = drumButtons.length;
 
+// Add click event listeners to each drum button
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let buttonInnerHTML = this.innerHTML;
+  drumButtons[i].addEventListener("click", function () {
+    const buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
 }
 
+// Add keydown event listener to the document
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
   buttonAnimation(event.key);
 });
 
 function makeSound(key) {
-  let sound;
   switch (key) {
     case "w":
-      sound = new Audio("sounds/tom-1.mp3");
+      new Audio("sounds/tom-1.mp3").play();
       break;
     case "a":
-      sound = new Audio("sounds/tom-2.mp3");
+      new Audio("sounds/tom-2.mp3").play();
       break;
     case "s":
-      sound = new Audio("sounds/tom-3.mp3");
+      new Audio("sounds/tom-3.mp3").play();
       break;
     case "d":
-      sound = new Audio("sounds/tom-4.mp3");
+      new Audio("sounds/tom-4.mp3").play();
       break;
     case "j":
-      sound = new Audio("sounds/snare.mp3");
+      new Audio("sounds/snare.mp3").play();
       break;
     case "k":
-      sound = new Audio("sounds/crash.mp3");
+      new Audio("sounds/crash.mp3").play();
       break;
     case "l":
-      sound = new Audio("sounds/kick-bass.mp3");
+      new Audio("sounds/kick-bass.mp3").play();
       break;
     default:
-      console.log(key); // Log key instead of buttonInnerHTML
+      console.log(key); // Log key if it's not a valid drum key
       return; // Exit function if no valid key
   }
-  if (sound) sound.play();
 }
 
 function buttonAnimation(currentKey) {
-  let activeButton = document.querySelector("." + currentKey);
+  const activeButton = document.querySelector("." + currentKey);
   if (activeButton) {
     // Check if activeButton exists
     activeButton.classList.add("pressed");
