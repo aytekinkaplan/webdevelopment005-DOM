904ddd6d68f0bc39cda37cd6acddadbccf74673b
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 08:20:40 +0300

    Commit

---
+++ project006/index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", function () {
+  document.querySelector("h1").textContent = "Goodbye!";
+  document.querySelector("h1").style.color = "blue";
+  document.querySelector("h1").style.fontSize = "60px";
+});
