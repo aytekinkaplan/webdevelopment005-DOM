43ddb0162773d5287b469eed65d7867596477538
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-11 11:30:53 +0300

    Commit

---
+++ Challenge/project003.ts
@@ -0,0 +1,7 @@
+//Understanding Callbacks and Event Listeners with examples
+//Example001
+const myButton = document.querySelector("button");
+myButton.addEventListener("click", () => {
+    console.log("Hello World!");
+})
+
